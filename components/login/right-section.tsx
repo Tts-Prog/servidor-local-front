7435590d7bed42053853e2fe0c438080c75a3e4f
TiagoSoares2334 <elvizoarez1@gmail.com>
2026-05-04 09:36:48 -0100

    feat: add state management and login API integration to the authentication form

--- components/login/right-section.tsx
+++ components/login/right-section.tsx
@@ -1,12 +1,55 @@
+"use client";
+
 import Link from "next/link";
 import { Button } from "../ui/button";
 import { Card, CardContent, CardHeader } from "../ui/card";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
+import { useState } from "react";
 
 export const RightSection = () => {
+  // useState
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const changeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value) {
+      setEmail(e.target.value);
+    } else {
+      setEmail("");
+    }
+  };
+
+  const changePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value) {
+      setPassword(e.target.value);
+    } else {
+      setPassword("");
+    }
+  };
+
+  const handleLogin = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    // fetch API
+    await fetch("http://localhost:8080/users/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    }).then((response) => {
+      console.log(response.json());
+    });
+  };
+
+  console.log({ email: email, password: password });
+
   return (
-    <div className="w-1/2  flex flex-col justify-center">
+    <div className="w-1/2 flex flex-col justify-center">
       <Card className="h-full flex flex-col justify-center px-14 gap-16">
         <CardHeader>
           <span className="text-5xl font-bold">Login</span>
@@ -20,6 +63,8 @@ export const RightSection = () => {
                 type="text"
                 placeholder="example@example.com"
                 className="py-2 h-10 text-lg"
+                value={email}
+                onChange={changeEmail}
               />
             </div>
 
@@ -29,10 +74,15 @@ export const RightSection = () => {
                 type="password"
                 placeholder="Your password..."
                 className="py-2 text-lg h-10"
+                value={password}
+                onChange={changePassword}
               />
             </div>
 
-            <Button className="bg-[#13A4EC] rounded-md text-white font-bold py-3 drop-shadow-lg drop-shadow-gray-200">
+            <Button
+              onClick={handleLogin}
+              className="bg-[#13A4EC] rounded-md text-white font-bold py-3 drop-shadow-lg drop-shadow-gray-200"
+            >
               Login
             </Button>
           </div>
