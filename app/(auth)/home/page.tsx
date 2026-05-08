758a0cd0f94b6e3f2ad55de3a2c65ab74ffad96a
TiagoSoares2334 <elvizoarez1@gmail.com>
2026-05-08 12:24:27 -0100

    feat: integrate Apollo Client and implement dynamic service listing on the home page

--- app/(auth)/home/page.tsx
+++ app/(auth)/home/page.tsx
@@ -1,19 +1,25 @@
 import { PedidoCard } from "@/components/core/pedido-card";
+import { getAllPrestacoesServicos } from "@/data/queries/prestacao-servico";
 
 export default function HomePage() {
+  const { loading, error, data } = getAllPrestacoesServicos();
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
   return (
     <div>
       <h1>Home</h1>
-      <PedidoCard
-        title="Instalar porta de entrada"
-        desciption="preciso instalar uma porta de entrada em uma casa de alvenaria na quadra 10 lote 14 seto b"
-        image="/placeholder.png"
-        category={{
-          id: "1",
-          nome: "Construção Civil",
-          icone: "/icone-placeholder.png",
-        }}
-      />
+      {data?.map((prestacaoServico) => (
+        <PedidoCard
+          key={prestacaoServico.id}
+          id={prestacaoServico.id}
+          title={prestacaoServico.designacao}
+          desciption={prestacaoServico.designacao}
+          image="/placeholder.jpeg"
+          category={prestacaoServico.servico.categoria}
+        />
+      ))}
     </div>
   );
 }
--- components/core/pedido-card.tsx
+++ components/core/pedido-card.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
-import { Card, CardContent } from "../ui/card";
+import { Card, CardContent, CardHeader } from "../ui/card";
+import { Button } from "../ui/button";
+import Link from "next/link";
 
 interface CategoriaType {
   id: string;
@@ -8,6 +10,7 @@ interface CategoriaType {
 }
 
 interface PedidoCardProps {
+  id: string;
   title: string;
   desciption: string;
   image: string;
@@ -16,10 +19,41 @@ interface PedidoCardProps {
 
 export const PedidoCard = (pedidoCardProps: PedidoCardProps) => {
   return (
-    <Card>
+    <Card className="h-[400px] w-full">
+      <CardHeader>
+        <div className="w-full h-[200px] relative">
+          <Image
+            src={pedidoCardProps.image}
+            alt={pedidoCardProps.title}
+            fill
+            className="object-cover"
+          />
+
+          <div className="absolute top-1/2 left-1/2 bg-secondary/80 text-secondary-foreground px-2 py-1 rounded-full text-sm">
+            <Image
+              src={pedidoCardProps.category.icone}
+              alt={pedidoCardProps.category.nome}
+              width={20}
+              height={20}
+            />
+          </div>
+        </div>
+      </CardHeader>
       <CardContent>
-        <div className="">
-          <Image src={pedidoCardProps.image} alt={pedidoCardProps.title} fill />
+        <div>
+          <h1>{pedidoCardProps.title}</h1>
+          <p>{pedidoCardProps.desciption}</p>
+        </div>
+
+        <div className="flex w-full items-center justify-between">
+          <div>
+            <span>Average Price:</span>
+            <span>$100</span>
+          </div>
+
+          <Link href={`/pedido/${pedidoCardProps.id}`} className=" ">
+            Explore
+          </Link>
         </div>
       </CardContent>
     </Card>
