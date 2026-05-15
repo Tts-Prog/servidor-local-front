e6d09a431496f58e9684b7a417bbb3ca9342891e
TiagoSoares2334 <elvizoarez1@gmail.com>
2026-05-15 11:33:09 -0100

    refactor: import useEffect to the login component to support side effect management

--- components/login/right-section.tsx
+++ components/login/right-section.tsx
@@ -5,7 +5,7 @@ import { Button } from "../ui/button";
 import { Card, CardContent, CardHeader } from "../ui/card";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { toast } from "sonner";
 
 import { setCookie } from "nookies";
